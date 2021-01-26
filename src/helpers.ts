4da6013779f9f5f320a959ef4cb63a2f2bf14d1d
Martin Beták <martin.betak@scrive.com>
2021-01-26 21:57:58 +0100

    Correctly handle empty attr Fixes #39

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,17 +1,22 @@
 import {
-  fromPredicate, mapNullable, getOrElse, Option,
+  fromPredicate, chain, mapNullable, getOrElse, Option,
 } from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/pipeable";
 
 import { NOT_MODIFIED_ENV } from "./constants";
 
 export const getShellCmd = (internalCommand: "env", attr: Option<string>) => (path: string) => {
+  const nonEmpty = fromPredicate<string>(
+    str => str !== null && str.trim() !== "",
+  );
+
   const toOption = fromPredicate<string>(
     configPath => configPath !== NOT_MODIFIED_ENV,
   );
 
   const attrArg = pipe(
     attr,
+    chain(nonEmpty),
     mapNullable(attr => `-A ${attr}`),
     getOrElse(() => ""),
   );
