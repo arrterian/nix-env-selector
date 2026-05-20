ee36601224dbdf6bbd4ed507cabc172794e1d09c
Niklas Hambüchen <mail@nh2.me>
2026-05-20 18:23:18 +0000

    Update build tools nixpkgs 24.05 -> 25.11

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.11.tar.gz") {} }:
 
 let
   src = pkgs.lib.cleanSourceWith {
